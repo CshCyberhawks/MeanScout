d93853c518a858192daf6368a4aa71bdfaf3183a
Ryan <pyan4444@gmail.com>
2022-10-26 16:15:42 -0400

    Updates

--- gh-pages.js
+++
@@ -1,9 +0,0 @@
-var ghpages = require("gh-pages");
-
-ghpages.publish(
-  "./gh-pages", // path to public directory
-  {
-    branch: "main",
-    repo: "https://github.com/CshCyberhawks/MeanScout.git", // Update to point to your repository
-  }
-);
