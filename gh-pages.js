a5982158f0f361a7d4d9a549bf4adf0874bf0d23
Ryan <pyan4444@gmail.com>
2022-10-26 16:07:50 -0400

    Made it actually deploy to the right spot

--- gh-pages.js
+++ gh-pages.js
@@ -1,7 +1,7 @@
 var ghpages = require("gh-pages");
 
 ghpages.publish(
-  "./public", // path to public directory
+  "./gh-pages", // path to public directory
   {
     branch: "main",
     repo: "https://github.com/CshCyberhawks/MeanScout.git", // Update to point to your repository
